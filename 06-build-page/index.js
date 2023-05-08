12b1b950146ef90cbecd2b673db4fb75f8088dcf
potopus <paveldelo@yandex.ru>
2023-05-09 02:08:26 +0300

    feat: begin resolve 6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,6 +3,12 @@ const path = require('path');
 const templateHTML = path.join(__dirname, 'template.html');
 const destinationFolder = path.join(__dirname, './project-dist');
 const sourceFolder = path.join(__dirname, './components');
+const arrTempl = ['header', 'articles', 'footer'];
+const templateVars = {
+  '{{header}}': 'header.html',
+  '{{articles}}': 'articles.html',
+  '{{footer}}': 'footer.html',
+};
 
 function createBundle(sourceFolder, destinationFolder) {
   // Создаем новую папку 'project-dist'
@@ -12,13 +18,14 @@ function createBundle(sourceFolder, destinationFolder) {
       return;
     }
     // Получаем список файлов в папке 'components'
-    fs.readdir(sourceFolder, (err, files) => {
+    fs.readdir(sourceFolder, (err, filesArr) => {
       if (err) {
         console.error(err);
         return;
       }
-
-      // Удаляем все файлы из папки 'files-copy'
+      // сортируем файлы по шаблону
+      let sortFiles = sortArrFiles(filesArr, arrTempl);
+      // Удаляем все файлы из папки 'project-dist'
       fs.readdir(destinationFolder, (err, destinationFiles) => {
         if (err) {
           console.error(err);
@@ -35,11 +42,23 @@ function createBundle(sourceFolder, destinationFolder) {
             console.log(`Файл ${path.parse(filePath).name} удален`);
           });
         });
-
         
+
+        console.log("Отсортированные: " + sortFiles);
       })
+
     })
   })
+}
+
+function sortArrFiles(filesArr, arrTempl) {
+  let sortFiles = [];
+  arrTempl.forEach((elem) => {
+    filesArr.forEach((file) => {
+      if (file.includes(elem)) { sortFiles.push(file) }
+    })
+  })
+  return sortFiles;
 }
 
 
