9059058750b46841973ff607bb5f9b6848d60d5a
potopus <paveldelo@yandex.ru>
2023-05-11 03:17:18 +0300

    fix: 6 task fixes for adding a list of files

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -9,102 +9,105 @@ const assetsFolder = path.join(__dirname, './assets');
 const assetsDestinationFolder = path.join(destinationFolder, './assets');
 
 const arrTempl = ['header', 'main', 'footer'];
-const templateVars = {
-  'header.html': '{{header}}',
-  'articles.html': '{{articles}}',
-  'footer.html': '{{footer}}'
-};
+
 
 
 async function createBundle(sourceFolder, destinationFolder) {
   // Создаем новую папку 'project-dist'
-  fs.mkdir(destinationFolder, { recursive: true }, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+  await fsp.mkdir(destinationFolder, { recursive: true });
 
-    // Получаем список файлов в папке 'components'
-    fs.readdir(sourceFolder, (err, filesHTML) => {
-      if (err) {
-        console.error(err);
-        return;
+
+  // Получаем список файлов в папке 'components'
+  let filesHTML = await fsp.readdir(sourceFolder);
+
+
+
+  // Удаляем все файлы из папки 'project-dist'
+  await clearFolder(destinationFolder)
+
+    console.log('Удаление выполнено работаем дальше: ');
+
+  
+
+    // собираем файл html
+    // прочитать файл template.html в переменную
+    let data= await fsp.readFile(templateHTML, 'utf8');
+
+      console.log('Сборка HTML');
+      // Получить список переменных
+      const templateVars = getVarsFromComponentsFolder(filesHTML);
+      // создаю цикл для чтения файла из каталога components
+      
+      const fileData = {};
+      for (const filename of filesHTML) {
+        try {
+          const data = await fsp.readFile(path.join(sourceFolder,filename), 'utf-8');
+          const key = filename.replace(/\..+$/, ''); // получаем имя файла без расширения
+          fileData[key] = data; // добавляем данные файла в объект
+        } catch (err) {
+          console.error(`Error reading file: ${filename}`);
+          throw err;
+        }
       }
-
-      // Удаляем все файлы из папки 'project-dist'
-      clearFolder(destinationFolder).then(() => {
-
-        console.log("Удаление выполнено работаем дальше: ")
-
-      }).then(() => {
-
-        // собираем файл html
-        // прочитать файл template.html в переменную
-        fs.readFile(templateHTML, 'utf8', (err, data) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-
-          console.log("Сборка HTML");
-          // создаю цикл для чтения файла из каталога components
-
-          for (let tempFile of filesHTML) {
-            console.log("цикл чтения файлов html")
-            // читаю файл tempFile и записываю его в переменную varCode
-            readFileIntoVariable(path.join(sourceFolder, tempFile)).then(function (result) {
-              // получить нужный шаблон, согласно используемого файла
-              let temple = templateVars[tempFile];
-              let regexp = new RegExp(`${temple}`, 'g');
-              // заменяю шалон {{...}} на текст из соответствующего файла
-              data = data.replace(regexp, `\n${result}`);
-              return data
-            }).then((result) => {
-              if (result.includes("!DOCTYPE")) {
-                writeFileFromVariable(path.join(destinationFolder, './index.html'), result).then(() => {
-                  console.log('File successfully written.');
-                }).catch((err) => {
-                  console.error(`Error writing file: ${err}`);
-                });
-              }
-            });
-          }
+      // console.log(fileData);
+       filesHTML.forEach((fileHtml)=> {
+        console.log('цикл чтения файлов html');
+        // читаю файл tempFile и записываю его в переменную varCode
+        // readFileIntoVariable(path.join(sourceFolder, tempFile))
+          // const tempFile = await fsp.readFile(path.join(sourceFolder,fileHtml), 'utf8');
+          // получить нужный шаблон, согласно используемого файла
+          // console.log(tempFile);
+          const key = fileHtml.replace(/\..+$/, ''); // получаем имя файла без расширения
+          let temple = templateVars[fileHtml];
+          let regexp = new RegExp(`${temple}`, 'g');
+          // заменяю шалон {{...}} на текст из соответствующего файла
+          data =  data.replace(`{{${key}}}`, `\n${fileData[key]}`);
+          
         })
-        return "text";
-      }).then((result) => {
-        console.log('CSS ' + result);
-        // получаем-читаем список файлов из папки styles
-        fs.readdir(cssFolder, (err, files) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          // сортирую файлы CSS по шаблону
-          let sortStyleFiles = sortArrFiles(files, arrTempl);
-          // добавляю слияание, как в 5 задании
-          const targetFile = path.join(destinationFolder, 'style.css');
-          const fileWriteStream = fs.createWriteStream(targetFile); // Создаем доступный для записи поток
-          // добавляю импортированную функцию
-          streamMergeRecursive(sortStyleFiles, cssFolder, fileWriteStream);
-        });
-      }
-      ).then(() => {
-        console.log('copy folder');
-        copyFolder(assetsFolder, assetsDestinationFolder).catch(error => console.error(error));
+        await fsp.writeFile(path.join(destinationFolder, './index.html'), data);
+        // if (result.includes('!DOCTYPE')) {
+          //   writeFileFromVariable(path.join(destinationFolder, './index.html'), result)
+          // }
+          // console.log(data);
+      
+    
+    
+ 
+    console.log('CSS ' );
+    // получаем-читаем список файлов из папки styles
+   let files = await fsp.readdir(cssFolder); 
+      // сортирую файлы CSS по шаблону
+      let sortStyleFiles = sortArrFiles(files, arrTempl);
+      // добавляю слияание, как в 5 задании
+      const targetFile = path.join(destinationFolder, 'style.css');
+      const fileWriteStream = fs.createWriteStream(targetFile); // Создаем доступный для записи поток
+      // добавляю импортированную функцию
+    await  streamMergeRecursive(sortStyleFiles, cssFolder, fileWriteStream);
+    
+ 
+  
+    console.log('copy folder');
+    copyFolder(assetsFolder, assetsDestinationFolder).catch(error => console.error(error));
 
-      }).catch(console.error);
-    });
-  });
+ 
+
 }
 
 async function writeFileFromVariable(filePath, data) {
-  try {
-    await fsp.writeFile(filePath, data);
-  } catch (err) {
-    throw err;
-  }
+
+  await fsp.writeFile(filePath, data);
+
 }
 
+function getVarsFromComponentsFolder(filesHTML) {
+  let objectVars = {};
+  const nameRegex = /^.*(?=\..*)/;
+  for (const file of filesHTML) {
+    const name = file.match(nameRegex)[0];
+    objectVars[file] = `{{${name}}}`;
+  }
+  return objectVars;
+}
 
 async function copyFolder(source, destination) {
   try {
@@ -129,17 +132,15 @@ async function copyFolder(source, destination) {
 }
 
 async function readFileIntoVariable(filePath) {
-  try {
-    const result = await fsp.readFile(filePath, 'utf8');
-    return result;
-  } catch (err) {
-    throw err;
-  }
+
+  const result = await fsp.readFile(filePath, 'utf8');
+  return result;
+
 }
 
 
 async function clearFolder(folderPath) {
-  console.log("работает удаление");
+  console.log('работает удаление');
   const files = await fsp.readdir(folderPath); // читаем содержимое папки
   for (const file of files) {
     const filePath = path.join(folderPath, file); // получаем полный путь к файлу/папке
